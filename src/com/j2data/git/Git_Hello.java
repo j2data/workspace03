c89fa88bc31dcb9d73bbdc329f61e9a28100f754
j2data <743746294@qq.com>
2020-08-05 16:23:45 +0800

    init git_hello project

---
+++ src/com/j2data/git/Git_Hello.java
@@ -0,0 +1,11 @@
+package com.j2data.git;
+
+public class Git_Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
+
